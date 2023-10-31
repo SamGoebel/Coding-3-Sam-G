d790b9329bf46cfc4da301f0ac1c2350f628a18b
SamGoebel <zxtra01@gmail.com>
2023-10-31 00:18:35 -0500

    Bible HW 2 (Revised)
    I forgot to add comments in certain sections

--- Bible-Assignment 2/src/BA2.java
+++ Bible-Assignment 2/src/BA2.java
@@ -6,7 +6,7 @@ public class BA2 {
         BigInteger initialDisciples = new BigInteger("13"); // Initial number of disciples
         int newDisciplesPerCouple = 1; // Number of new disciples per couple
         int trainingAge = 18; // Age when disciples can start training
-        int newDiscipleAge = 21; // Age when male disciples can start training new disciples
+        int newDiscipleAge = 21; // Age when disciples can start training new disciples
         int newDiscipleFrequency = 3; // New disciple training frequency
 
         BigInteger disciples = initialDisciples; // Initialize with the initial disciples
@@ -41,18 +41,17 @@ public class BA2 {
                 break; //ends the program so it doesn't keep going. 
             }
 
-            // Check if there is a couple of disciples, a man and a woman, who are 30 years old
+            // Checks if there is a couple of disciples who are both 30 years old
             if (years >= 30) {
                 BigInteger maleDisciples = disciples.divide(BigInteger.valueOf(2));
                 BigInteger femaleDisciples = disciples.divide(BigInteger.valueOf(2));
 
                 if (maleDisciples.compareTo(BigInteger.ZERO) > 0 && femaleDisciples.compareTo(BigInteger.ZERO) > 0) {
-                    // A couple of disciples, a man and a woman, who are 30 years old, have a child
-                    disciples = disciples.add(BigInteger.ONE);
+                   disciples = disciples.add(BigInteger.ONE);
                 }
             }
 
-            // Remove disciples when they reach the age of 72
+            // Remove disciples when they reach the age of 72 because they died :(
             if (years % 72 == 0) {
                 BigInteger elderlyDisciples = disciples.divide(BigInteger.valueOf(2));
                 disciples = disciples.subtract(elderlyDisciples);
